70c0f1e96531842153ce81d47b8f70c00d4ac2de
cecho0 <15905922153@163.com>
2023-12-03 00:38:16 +0800

    update

--- setup.py
+++ setup.py
@@ -17,6 +17,7 @@ from requests.exceptions import SSLError
 from requests.exceptions import ProxyError
 from requests.exceptions import ConnectTimeout
 from requests.exceptions import ConnectionError
+from pathlib import Path
 from tqdm import tqdm
 
 proxy = {
@@ -24,18 +25,7 @@ proxy = {
     "https": "",
 }
 
-global_config = {
-    "user": os.getlogin(),
-    "arch": platform.machine(),
-    "system": platform.system(),
-    "tmp_path": os.path.dirname(os.path.realpath(__file__)) + "/.tmp_cache",
-    "tmp_bin_path": os.path.dirname(os.path.realpath(__file__)) + "/.tmp_cache/bin",
-    "tmp_software_path": os.path.dirname(os.path.realpath(__file__)) + "/.tmp_cache/software",
-    "home_path": os.path.expanduser("~"),
-    "config_path": os.path.expanduser("~") + "/.config",
-    "root_path": os.path.dirname(os.path.realpath(__file__)),
-    "install_type": "soft",
-}
+g_config = {}
 
 # ANSI 转义码
 reset = "\033[0m"      # reset
@@ -58,28 +48,63 @@ def info_log(text):
     print(f"{bold}{text}{reset}")
 
 def init_env():
-    if os.path.exists(global_config["tmp_path"]) and os.path.isdir(global_config["tmp_path"]):
-        shutil.rmtree(global_config["tmp_path"])
-    os.mkdir(global_config["tmp_path"])
-    os.mkdir(global_config["tmp_bin_path"])
-    os.mkdir(global_config["tmp_software_path"])
+    try:
+        g_config["user"] = os.getlogin()
+        g_config["arch"] = platform.machine()
+        g_config["system"] = platform.system()
+        g_config["tmp_path"] = os.path.dirname(os.path.realpath(__file__)) + "/.tmp_cache"
+        g_config["tmp_bin_path"] = os.path.dirname(os.path.realpath(__file__)) + "/.tmp_cache/bin"
+        g_config["tmp_software_path"] = os.path.dirname(os.path.realpath(__file__)) + "/.tmp_cache/software"
+
+        if g_config["system"] != "Linux":
+            error_log("this script only support linux now.")
+            sys.exit()
+
+        user = pwd.getpwnam(g_config["user"])
+        g_config["home_path"] = user.pw_dir
+        g_config["config_path"] = user.pw_dir + "/.config"
+        g_config["root_path"] = os.path.dirname(os.path.realpath(__file__))
+        g_config["install_type"] = "soft"
+
+        success_log("arch: " + g_config["arch"])
+        success_log("system: " + g_config["system"])
+        success_log("home: " + g_config["home_path"])
+        success_log("config: " + g_config["config_path"])
+        success_log("root: " + g_config["root_path"])
+        success_log("install type: " + g_config["install_type"])
+
+    except FileNotFoundError as e:
+        error_log("please use command 'sudo' to exec this script.")
+        sys.exit()
+
+    if os.path.exists(g_config["tmp_path"]):
+        shutil.rmtree(g_config["tmp_path"])
+    os.mkdir(g_config["tmp_path"])
+    os.mkdir(g_config["tmp_bin_path"])
+    os.mkdir(g_config["tmp_software_path"])
+
+    if not os.path.exists(g_config["config_path"]):
+        os.mkdir(g_config["config_path"])
 
 def check_config_valid(data):
-    if data["install_type"] == "soft" or data["install_type"] == "hard":
-        return True
-    error_log("option 'install_type' only support 'soft' or 'hard'")
-    sys.exit()
+    if data["install_type"] != "soft" and data["install_type"] != "hard":
+        error_log("option 'install_type' only support 'soft' or 'hard'")
+        sys.exit()
+
+    if not os.path.exists(g_config["home_path"]):
+        error_log(f"home path format error")
+        sys.exit()
 
 def load_config():
     filename = os.path.dirname(os.path.realpath(__file__)) + "/config.yml"
 
-    if global_config["arch"].lower() != "x86_64" or global_config["system"].lower() != "linux":
+    if g_config["arch"].lower() != "x86_64" or g_config["system"].lower() != "linux":
         error_log("only support x86_64 linux")
         sys.exit()
 
     # read data from yaml file
     if not os.path.exists(filename):
-        error_log("config file: config.yml can't find")
+        error_log(f"config file: {filename} can't find")
         sys.exit()
 
     try:
@@ -91,17 +116,7 @@ def load_config():
                 url = data["proxy"]
                 proxy["http"] = f"http://{url}"
                 proxy["https"] = f"http://{url}"
-            global_config["install_type"] = data["install_type"]
-            user = pwd.getpwnam(global_config["user"])
-            global_config["home_path"] = user.pw_dir
-            global_config["config_path"] = user.pw_dir + "/.config"
-
-            if not os.path.exists(global_config["home_path"]):
-                error_log(f"home path format error")
-                sys.exit()
-
-            if not os.path.exists(global_config["config_path"]):
-                os.mkdir(global_config["config_path"])
+            g_config["install_type"] = data["install_type"]
 
             return data
     except yaml.parser.ParserError as e:
@@ -158,10 +173,10 @@ def get_version_links(key, username, repo, token, link_format):
             return None
 
         release_info = response.json()
-        latest_version = release_info['tag_name']
+        latest_version = release_info["tag_name"]
 
         # get all download links
-        links = [asset['browser_download_url'] for asset in release_info['assets']]
+        links = [asset["browser_download_url"] for asset in release_info["assets"]]
         if "version" in link_format:
             link_format = link_format.format(version = latest_version)
 
@@ -173,6 +188,7 @@ def get_version_links(key, username, repo, token, link_format):
         error_log(f"{key} get download link failed.")
         return None
     except (SSLError, ProxyError, ConnectTimeout, ConnectionError )as e:
+        error_log(f"SSL error: {e}")
         return None
     except OSError as e:
         error_log(f"OS error: {e}")
@@ -201,11 +217,11 @@ def download_file(key, link):
             error_log("get file content failed.")
             return None
 
-        local_file = global_config["tmp_path"] + "/package"
+        local_file = g_config["tmp_path"] + "/package"
         if os.path.exists(local_file) and not os.path.isdir(local_file):
             os.remove(local_file)
 
-        with open(global_config["tmp_path"] + "/package", "wb") as file:
+        with open(g_config["tmp_path"] + "/package", "wb") as file:
             for data in file_response.iter_content(chunk_size = 1024):
                 file.write(data)
                 progress_bar.update(len(data))
@@ -225,35 +241,35 @@ def download_file(key, link):
 def handle_file(key, local_file, extension):
     if extension == ".gz":
         with tarfile.open(local_file, "r:gz") as tar:
-            tar.extractall(global_config["tmp_software_path"])
+            tar.extractall(g_config["tmp_software_path"])
     elif extension == ".zip":
         with zipfile.ZipFile(local_file, "r") as zip_ref:
-            zip_ref.extractall(global_config["tmp_software_path"])
+            zip_ref.extractall(g_config["tmp_software_path"])
     else:
         error_log(f"unsupport format {extension}")
         return
 
     if key != "nvim":
         # copy file
-        for root, dirs, files in os.walk(global_config["tmp_software_path"]):
+        for root, dirs, files in os.walk(g_config["tmp_software_path"]):
             for file_name in files:
                 if file_name == key:
                     target_file_path = os.path.join(root, file_name)
-                    shutil.copy(target_file_path, global_config["tmp_bin_path"] + "/" + key)
+                    shutil.copy(target_file_path, g_config["tmp_bin_path"] + "/" + key)
     else:
-        # neovim need cp whole directory
-        for root, dirs, files in os.walk(global_config["tmp_software_path"]):
+        # neovim need copy whole directory
+        for root, dirs, files in os.walk(g_config["tmp_software_path"]):
             for directory in dirs:
                 if "nvim" in directory:
                     target_dir_path = os.path.join(root, directory)
-                    shutil.move(target_dir_path, global_config["tmp_bin_path"] + "/" + key)
+                    shutil.move(target_dir_path, g_config["tmp_bin_path"] + "/" + key)
 
     # clean dir
-    if os.path.exists(local_file) and not os.path.isdir(local_file):
+    if os.path.exists(local_file):
         os.remove(local_file)
-    if os.path.exists(global_config["tmp_software_path"]) and os.path.isdir(global_config["tmp_software_path"]):
-        shutil.rmtree(global_config["tmp_software_path"])
-        os.mkdir(global_config["tmp_software_path"])
+    if os.path.exists(g_config["tmp_software_path"]):
+        shutil.rmtree(g_config["tmp_software_path"])
+        os.mkdir(g_config["tmp_software_path"])
 
 def download(data):
     if not data["download"]["enable"]:
@@ -261,7 +277,7 @@ def download(data):
 
     for key in data["software"]:
         if check_command(key):
-            info_log(f"{key} exist skip...")
+            info_log(f"{key} exist, skip...")
             continue
 
         username = data["software"][key]["username"]
@@ -307,14 +323,12 @@ def download(data):
             sys.exit()
         except OSError as e:
             error_log(f"OS error: {e}")
-            traceback.print_exc()
             sys.exit()
         except Exception as e:
             error_log(f"error: {e}")
-            traceback.print_exc()
             sys.exit()
-    # start install software
 
+    # start install software
     success_log("software download finished.")
 
 def install(data):
@@ -331,13 +345,13 @@ def install(data):
         if not os.path.exists(bin_path):
             os.mkdir(bin_path)
 
-        for filename in os.listdir(global_config["tmp_bin_path"]):
+        for filename in os.listdir(g_config["tmp_bin_path"]):
             if filename in sp_list:
                 sp_path = sp_list[filename]["install"]
                 if "home" in sp_path:
-                    sp_path = sp_path.format(home = global_config["home_path"])
+                    sp_path = sp_path.format(home = g_config["home_path"])
                 elif "config" in sp_path:
-                    sp_path = sp_path.format(home = global_config["config_path"])
+                    sp_path = sp_path.format(home = g_config["config_path"])
                 else:
                     error_log(f"{sp_path} path formatch error")
                     continue
@@ -348,8 +362,8 @@ def install(data):
                 if os.path.exists(ins_dst) and os.path.isdir(ins_dst):
                     shutil.rmtree(ins_dst)
 
-                shutil.move(global_config["tmp_bin_path"] + "/" + filename, ins_dst)
-                user = pwd.getpwnam(global_config["user"])
+                shutil.move(g_config["tmp_bin_path"] + "/" + filename, ins_dst)
+                user = pwd.getpwnam(g_config["user"])
                 mod_uid_gid(ins_dst, user.pw_uid, user.pw_gid)
 
                 link_src = sp_path + "/" + filename + "/" + sp_list[filename]["bin"]
@@ -357,7 +371,7 @@ def install(data):
                 mod_uid_gid(link_src, user.pw_uid, user.pw_gid)
 
             else:
-                shutil.move(global_config["tmp_bin_path"] + "/" + filename, bin_path + "/" + filename)
+                shutil.move(g_config["tmp_bin_path"] + "/" + filename, bin_path + "/" + filename)
                 mod_uid_gid(bin_path + "/" + filename, user.pw_uid, user.pw_gid)
 
     except OSError as e:
@@ -365,7 +379,6 @@ def install(data):
         sys.exit()
     except Exception as e:
         error_log(f"error: {e}")
-        traceback.print_exc()
         sys.exit()
 
     success_log("software install finished.")
@@ -374,7 +387,7 @@ def config_install(data):
     if not data["config"]["enable"]:
         return
 
-    user = pwd.getpwnam(global_config["user"])
+    user = pwd.getpwnam(g_config["user"])
     for key in data["config"]:
         if key == "enable":
             continue
@@ -386,19 +399,19 @@ def config_install(data):
         root_format = data["config"][key]["root_path"]
         config_path = None
         if "home_path" in config_format:
-            config_path = config_format.format(home_path = global_config["home_path"])
+            config_path = config_format.format(home_path = g_config["home_path"])
         elif "config_path" in config_format:
-            config_path = config_format.format(config_path = global_config["config_path"])
+            config_path = config_format.format(config_path = g_config["config_path"])
         else:
             error_log("error format {}", config_format)
             continue
 
-        root_path = root_format.format(root_path = global_config["root_path"])
+        root_path = root_format.format(root_path = g_config["root_path"])
         try:
-            if global_config["install_type"] == "soft":
+            if g_config["install_type"] == "soft":
                 os.symlink(root_path, config_path)
                 os.lchown(config_path, user.pw_uid, user.pw_gid)
-            if global_config["install_type"] == "hard":
+            if g_config["install_type"] == "hard":
                 shutil.copy(root_path, config_path)
                 mod_uid_gid(config_path, user.pw_uid, user.pw_gid)
         except OSError as e:
@@ -407,8 +420,8 @@ def config_install(data):
 
 def clean(data):
     # delete tmp directory
-    if os.path.exists(global_config["tmp_path"]) and os.path.isdir(global_config["tmp_path"]):
-        shutil.rmtree(global_config["tmp_path"])
+    if os.path.exists(g_config["tmp_path"]):
+        shutil.rmtree(g_config["tmp_path"])
 
 def main():
     init_env()
